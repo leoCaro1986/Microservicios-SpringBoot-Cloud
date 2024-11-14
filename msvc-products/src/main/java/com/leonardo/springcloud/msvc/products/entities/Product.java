ffc961c30a63a3538119912a8298f668c120c458
leonardo.caro <leonardo.caro@sofka.com.co>
2024-11-14 15:40:20 -0500

    Se crea proyecto inicial de productos

---
+++ msvc-products/src/main/java/com/leonardo/springcloud/msvc/products/entities/Product.java
@@ -0,0 +1,25 @@
+package com.leonardo.springcloud.msvc.products.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    @Column(name = "create_at")
+    private LocalDate createAt;
+}
