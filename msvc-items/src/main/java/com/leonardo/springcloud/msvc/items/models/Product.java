34f4d202bfb5ad49735a2aed2cf1f55aeb0c9378
leonardo.caro <leonardo.caro@sofka.com.co>
2024-11-15 11:11:26 -0500

    Se crea microservicio items y su estructura inicial

---
+++ msvc-items/src/main/java/com/leonardo/springcloud/msvc/items/models/Product.java
@@ -0,0 +1,36 @@
+package com.leonardo.springcloud.msvc.items.models;
+
+import java.time.LocalDate;
+
+public class Product {
+
+    private long id;
+    private String name;
+    private Double price;
+    private LocalDate createdAt;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
