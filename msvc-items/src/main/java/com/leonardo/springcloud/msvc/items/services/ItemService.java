962d3f6d5171b75e299e8d97c6acbce0c6985544
leonardo.caro <leonardo.caro@sofka.com.co>
2024-11-15 11:25:15 -0500

    Se crea interfaz del service para los items

---
+++ msvc-items/src/main/java/com/leonardo/springcloud/msvc/items/services/ItemService.java
@@ -0,0 +1,12 @@
+package com.leonardo.springcloud.msvc.items.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.leonardo.springcloud.msvc.items.models.Item;
+
+public interface ItemService {
+    List<Item> findAll();
+    Optional<Item> findById(Long id);
+
+}
