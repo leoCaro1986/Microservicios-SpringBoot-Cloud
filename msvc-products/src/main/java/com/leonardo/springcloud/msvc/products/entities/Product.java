403333b0e971aaf18b38d8e3873b083a5088fcaa
leonardo.caro <leonardo.caro@sofka.com.co>
2024-11-14 21:49:06 -0500

    Se crea controller, repositorio, service y config inicial para mysql

--- msvc-products/src/main/java/com/leonardo/springcloud/msvc/products/entities/Product.java
+++ msvc-products/src/main/java/com/leonardo/springcloud/msvc/products/entities/Product.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "productos")
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ msvc-products/src/main/java/com/leonardo/springcloud/msvc/products/services/ProductService.java
@@ -0,0 +1,13 @@
+package com.leonardo.springcloud.msvc.products.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.leonardo.springcloud.msvc.products.entities.Product;
+
+public interface ProductService {
+
+    List<Product> findAll();
+    
+    Optional<Product> findById(Long id);
+}
